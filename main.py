bc2f039af54f3a03dd6da4e23f729e52462abd99
mkim6993 <50627842+mkim6993@users.noreply.github.com>
2020-04-26 23:59:20 -0400

    modifications to main, title, and first question

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-from flask import Flask
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!"
+    return render_template('sample.html')
     
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run('0.0.0.0',port=3000)
