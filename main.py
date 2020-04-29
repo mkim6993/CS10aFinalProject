1d86300c263a43a2e7743b87aad5a465a8baaa57
mkim6993 <50627842+mkim6993@users.noreply.github.com>
2020-04-28 23:56:08 -0400

    updatees

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
-from flask import Flask, render_template, request
-app = Flask(__name__)
+from flask import Flask, redirect, url_for, render_template, request, send_from_directory
+import json
+
+app = Flask(__name__, template_folder='templates')
+
+
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+   # return render_template('main.html')
+   return render_template('firstQuestion.html')
 
-@app.route('/first')
+@app.route('/data')
+def data():
+    with open('questions.json') as json_file:
+        data = json.load(json_file)
+        return data 
+
+@app.route('/first',methods=['GET','POST'])
 def firstQuestion():
-	return render_template('firstQustion.html')
+    if request.method == "POST":
+        answer = request.form["q1"]
+
+    else:
+        return render_template('testQ.html')
+
+@app.route("/<usr>")
+def user(usr):
+    return f"<h1>{usr}</h1>"
 
 if __name__ == "__main__":
+    app.debug = True
     app.run('0.0.0.0',port=3000)
 
 
